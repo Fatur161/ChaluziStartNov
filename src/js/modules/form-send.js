8e01b5d7745bfe0f160089cb1587a5fed2d653ea
Daniil Semenov <danil_semenov18@mail.ru>
2024-06-19 20:01:20 +0300

    - fixes for cms

--- src/js/modules/form-send.js
+++ src/js/modules/form-send.js
@@ -1,21 +1,37 @@
 document.querySelector("#mainForm").addEventListener("submit", function (e) {
   e.preventDefault(); // Предотвращаем стандартную отправку формы
 
-  let formData = new FormData(this); // Создаем объект FormData из данных формы
-  let form = this;
-  if (form.checkValidity()) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", WPJS.siteUrl + "/assets/php/send_to_server.php"); // Указываем метод и путь к обработчику
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-        alert("Ваша заявка отправлена, в скором времени мы с вами свяжемся");
-      } else {
-        alert("Упс, что-то пошло не так");
-      }
-    };
-    xhr.send(formData); // Отправляем данные формы
+  if (!this.submitBlocked || !this.submitTimer) {
+    // Проверяем, не заблокирована ли отправка
+    this.submitBlocked = true; // Блокируем повторные отправки
+
+    let formData = new FormData(this); // Создаем объект FormData из данных формы
+    let form = this;
+    if (form.checkValidity()) {
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST", WPJS.siteUrl + "/assets/php/send_to_server.php"); // Указываем метод и путь к обработчику
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200) {
+            alert(
+              "Ваша заявка отправлена, в скором времени мы с вами свяжемся"
+            );
+          } else {
+            alert("Упс, что-то пошло не так");
+          }
+          clearTimeout(form.submitTimer); // Очищаем таймер
+          form.submitTimer = null; // Обнуляем таймер
+          form.submitBlocked = false; // Разрешаем повторные отправки
+        }
+      };
+      xhr.send(formData); // Отправляем данные формы
+
+      // Установка таймера для разрешения повторных отправок через 2 секунды
+      this.submitTimer = setTimeout(() => {
+        this.submitBlocked = false;
+      }, 2000);
+    }
   } else {
-    e.stopPropagation();
-    form.classList.add("was-validated");
+    alert("Подождите 2 секунды перед новой отправкой");
   }
 });
